e688ebc71604123ee35a1af0cb6efbd3ac2b75aa
vinoddotcom <vinod.yadav@vidyamantra.com>
2023-08-12 14:32:19 +0530

    Added test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,31 @@
-describe('template spec', () => {
+describe('Clear Form Test', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('http://localhost:5173/')
+    cy.get('[data-name="name"]').type('John Doe')
+    cy.get('[data-email="email"]').type('johndoe@example.com')
+    cy.get('#Fantastic').check()
+    cy.get('[data-clear="clear"]').click()
+    cy.get('[data-name="name"]').should('have.value', '')
+    cy.get('[data-email="email"]').should('have.value', '')
+  })
+})
+
+describe('Empty Form Fields Test', () => {
+  it('Should show an error message when form fields are empty', () => {
+    cy.visit('http://localhost:5173/')
+    cy.get('[data-submit="submit"]').click()
+    cy.get('[data-errorMsg="errorMsg"]').should('be.visible')
+  })
+})
+
+describe('On Submitting the filled Form Test', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:5173/')
+    cy.get('[data-name="name"]').type('John Doe')
+    cy.get('[data-email="email"]').type('johndoe@example.com')
+    cy.get('#Fantastic').check()
+    cy.get('[data-submit="submit"]').click()
+    cy.get('[data-success="success"]').should('be.visible')
+
   })
 })
