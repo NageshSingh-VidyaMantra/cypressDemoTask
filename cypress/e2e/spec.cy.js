05e8d75e504b6c4d0a820d69e242b12576bd4bee
suresh sharma <suresh.sharma510@gmail.com>
2023-08-12 14:54:10 +0530

    cypress test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,16 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Nagesh cypress test', () => {
+  it('inputs success test', () => {
+    cy.getByData("name").type("suresh");
+    cy.getByData("email").type("suresh@ghil.com");
+    cy.getByData("submit").click();
+    cy.getByData("submit").type("Submitted");
+    cy.getByData("success").type("**suresh@ghil.com successfully subscribed the newsletter.");
+  })
+
+  it('inputs fail test', () => {
+    cy.getByData("name").type("suresh");
+    cy.getByData("email").type("sureshghilcom");
+    cy.getByData("submit").click();
+    cy.getByData("errorMsg").type("**All fields are required.");
   })
 })
